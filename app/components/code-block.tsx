ffb192e7b0225b704d69485b19a898198437dc2a
Charlie Gleason <charlieg@replicate.com>
2025-10-07 20:23:42 -0700

    Tidied code examples

--- app/components/code-block.tsx
+++ app/components/code-block.tsx
@@ -1,4 +1,10 @@
-import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  Sandpack,
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackProvider,
+} from "@codesandbox/sandpack-react";
 import { nightOwl } from "@codesandbox/sandpack-themes";
 import type { ReactNode } from "react";
 
@@ -8,6 +14,29 @@ interface PreProps {
   code: string;
   [key: string]: ReactNode | string | Array<ReactNode | string>;
 }
+
+const css = `body {
+font-family: sans-serif;
+-webkit-font-smoothing: auto;
+-moz-font-smoothing: auto;
+-moz-osx-font-smoothing: grayscale;
+font-smoothing: auto;
+text-rendering: optimizeLegibility;
+font-smooth: always;
+-webkit-tap-highlight-color: transparent;
+-webkit-touch-callout: none;
+background-color: #000;
+}
+
+html,
+body,
+#root {
+height: 100%;
+}
+
+h1 {
+font-size: 1.5rem;
+}`;
 
 export default function CodeBlock({
   children,
@@ -46,11 +75,11 @@ export default function CodeBlock({
   console.log(code, dependencies);
 
   return (
-    <Sandpack
+    <SandpackProvider
       theme={nightOwl}
       files={{
         "/App.tsx": code,
-        "/index.css": `body { background-color: #000; }`,
+        "/styles.css": { code: css, hidden: true },
       }}
       options={{
         externalResources: ["https://cdn.tailwindcss.com"],
@@ -59,6 +88,15 @@ export default function CodeBlock({
       customSetup={{
         dependencies,
       }}
-    />
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor />
+        <SandpackPreview
+          showOpenInCodeSandbox={false}
+          showRefreshButton={false}
+          showOpenNewtab={false}
+        />
+      </SandpackLayout>
+    </SandpackProvider>
   );
 }
