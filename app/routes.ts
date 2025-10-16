37a81474773aa827ae36b4b1f04d74f389c1edb8
Charlie Gleason <charlieg@replicate.com>
2025-10-15 17:00:34 -0700

    CSS tweaks

--- app/routes.ts
+++ app/routes.ts
@@ -4,8 +4,8 @@ import { routes } from "./mdx/mdx-routes";
 export default [
   index("routes/index.tsx"),
   route("theme-switch", "routes/resources/theme-switch.tsx"),
-  route(":slug.png", "routes/resources/og-image.tsx"),
   route("rss", "routes/resources/rss.tsx"),
+  route(":slug.png", "routes/resources/og-image.tsx"),
   route("posts.json", "routes/resources/posts.tsx"),
   ...routes("routes/post.tsx"),
   route("*", "routes/404.tsx"),
