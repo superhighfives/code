2ed4b5edeb21ac0d67699ea26eb74a443659b9e7
Charlie Gleason <charlieg@replicate.com>
2025-10-16 17:14:28 -0700

    Fix command

--- app/components/command.tsx
+++ app/components/command.tsx
@@ -5,7 +5,7 @@ export default function Command({
 }) {
   return (
     <div
-      className="relative border p-4 rounded-sm shadow-md bg-gray-50 dark:bg-gray-950 border-gray-300 dark:border-gray-700 mt-4 break-words [&>pre>code]:whitespace-normal"
+      className="relative border p-4 rounded-sm shadow-md bg-gray-50 dark:bg-gray-950 border-gray-300 dark:border-gray-700 mt-4 break-words [&>pre>code]:whitespace-pre-wrap"
       // biome-ignore lint/security/noDangerouslySetInnerHtml: required by shiki
       dangerouslySetInnerHTML={{ __html: highlightedHtml }}
     />
